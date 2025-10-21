0d09202db83117073c038341ed01a4e6c12f717b
iqbal <iqbalbudinugroho@gmail.com>
2025-10-21 09:18:40 +0700

    upload gambar

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const bodyParser = require('body-parser');
-const Image = require('./models/image');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
-const { path } = require('express/lib/application');
+const Image = require('./models/Image');
+const { get } = require('express/lib/response');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true}));
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017/', {
+useNewUrlParser: true,
+useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+console.log('✅ MongoDB connected');
+});
+
+mongoose.connection.on('error', (err) => {
+console.error('❌ MongoDB connection error:', err);
+});
+
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/ukmDB');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'public/uploads'),
-    filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+destination: (req, file, cb) => cb(null, 'public/uploads'),
+filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
 });
 const upload = multer({ storage });
 
+
 app.get('/', async (req, res) => {
-    const Image = await Image.findOne().sort({ uploadAt: -1});
-    res.render('index', { Image });
-})
+const image = await Image.findOne().sort({ uploadedAt: -1 });
+res.render('index', { image });
+});
 
 app.get('/admin', (req, res) => {
-    res.render('admin');
-})
+res.render('admin');
+});
 
 app.post('/upload', upload.single('image'), async (req, res) => {
-    const newImage = new Image({
-        filename: req.file.filename,
-        path: '/uploads/' + req.file.filename
-    });
-    await newImage.save();
-    res.redirect('/');
-})
+const newImage = new Image({
+    filename: req.file.filename,
+    path: '/uploads/' + req.file.filename
+});
+await newImage.save();
+res.redirect('/');
+});
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app,get('/', async (req, res) => {
+    const image = await Image.findOne().sort({ uploadedAt: -1});
+    res.render('index', { image});
+});
+
+
+app.listen(3000, () => console.log('🚀 Server running on http://localhost:3000'));
